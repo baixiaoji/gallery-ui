11014a7af6e906a862d05eac827628bb87be4e7d
baixiaoji <1041874421@qq.com>
2019-11-14 14:52:10 +0800

    feat: 插件 button-group 的子元素是不是 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -6,8 +6,15 @@
 
 <script>
   export default {
-    name: '',
-    props: {},
+    name: 'gallery-button-group',
+    mounted() {
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase();
+        if (name !== 'button') {
+          console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${ name }`);
+        }
+      }
+    },
   };
 </script>
 
@@ -15,17 +22,24 @@
   .g-button-group {
     display: inline-flex;
     vertical-align: middle;
+    
     > .g-button {
       border-radius: 0;
-      margin-left: -1px;
+      
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      
       &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
       }
+      
       &:last-child {
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
       }
+      
       &:hover {
         position: relative;
         z-index: 1;
