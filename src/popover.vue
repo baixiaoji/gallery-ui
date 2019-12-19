f503980b5ee081d9ac761806a1164851bd5ded33
baixiaoji <1041874421@qq.com>
2019-12-19 15:42:26 +0800

    feat: 完成了popover可以正常冒泡click事件

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class='popover' ref='popover' @click.stop='onClickHandler'>
+  <div class='popover' ref='popover' @click='onClickHandler'>
     <div class="content-wrapper" ref='contentWrapper' v-show='visible' @click.stop>
       <slot name='content'></slot>
     </div>
-    <slot></slot>
+    <span ref='triggerWrapper' class='trigger-wrapper'>
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -16,20 +18,36 @@
       };
     },
     methods: {
-      onClickHandler() {
-        this.visible = !this.visible;
-        if (this.visible) {
-          document.body.append(this.$refs.contentWrapper);
-          const {top, left} = this.$refs.popover.getBoundingClientRect();
-          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
-          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
-          const documentListener = () => {
-            this.visible = false;
-            document.removeEventListener('click', documentListener);
-          };
-          this.$nextTick(() => {
-            document.addEventListener('click', documentListener);
-          });
+      positionContentWrapper() {
+        document.body.append(this.$refs.contentWrapper);
+        const {top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+      },
+      documentListener(e) {
+        if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+          return;
+        }
+        this.close()
+      },
+      open() {
+        this.visible = true;
+        this.$nextTick(() => {
+          this.positionContentWrapper();
+          document.addEventListener('click', this.documentListener);
+        });
+      },
+      close() {
+        this.visible = false;
+        document.removeEventListener('click', this.documentListener);
+      },
+      onClickHandler(event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible) {
+            this.close();
+          } else {
+            this.open();
+          }
         }
       },
     },
@@ -38,8 +56,13 @@
 
 <style scoped lang='scss'>
   .popover {
-    position: relative;
+    display: inline-block;
+    
+    .trigger-wrapper {
+      display: inline-block;
+    }
   }
+  
   .content-wrapper {
     position: absolute;
     border: 1px solid red;
