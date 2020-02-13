5303b5a1e06141eb4c15fa0d0089f4479307cf28
baixiaoji <1041874421@qq.com>
2020-02-13 15:10:53 +0800

    feat: 调整了迁移vue-cli过程中的demo页面

---
+++ src/demo.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div>eeeeeee</div>
+    <g-cascader :source='source' popover-height='300px'/>
+    <div>2222222222222</div>
+  </div>
+</template>
+
+<script>
+  import Cascader from './cascader/cascader.vue';
+  
+  export default {
+    components: {
+      'g-cascader': Cascader,
+    },
+    data(){
+      return {
+        source: [{
+          name: '浙江',
+          children: [
+            {
+              name: '杭州',
+              children: [
+                {name: '上城'},
+                {name: '下城'},
+                {name: '江干'},
+              ]
+            },
+            {
+              name: '嘉兴',
+              children: [
+                {name: '南湖'},
+                {name: '秀洲'},
+                {name: '嘉善'},
+              ]
+            },
+          ]
+        }, {
+          name: '福建',
+          children: [
+            {
+              name: '福州',
+              children: [
+                {name: '鼓楼'},
+                {name: '台江'},
+                {name: '仓山'},
+              ]
+            },
+          ]
+        }, {
+          name: '安徽',
+          children: [{
+            name: '合肥',
+            children: [{
+              name: '瑶海'
+            }, {
+              name: '庐阳'
+            }]
+          }]
+        }],
+      };
+    },
+    props: {},
+    methods: {},
+  };
+</script>
+
+<style lang='scss'>
+  * {margin: 0; padding: 0; box-sizing: border-box;}
+  :root {
+    --button-height: 32px;
+    --font-size: 14px;
+  }
+  body {
+    font-size: var(--font-size);
+    padding: 100px;
+    border: 1px solid black;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Demo from './demo.vue';
+
+new Vue({
+  render: h => h(Demo),
+}).$mount('#app');
